6f2e137ab6051c57fb33264a74b52322a19b347a
duasarmad <163379152+duasarmad@users.noreply.github.com>
2024-05-09 08:45:05 -0700

    Add files via upload

---
+++ new1.js
@@ -0,0 +1,4 @@
+console.log("dua");
+console.log(window);//This line will not execute on node js
+
+
---
+++ varlet.js
@@ -0,0 +1,10 @@
+var name = "dua";
+var name ="dua"; //can be declare again
+var name ="dee"; //can modify also
+
+let age =10; //cant declare same variable with let but can modify
+const salary =100;
+age=61;
+//const salary=100; //cant declare const again and can modify value
+
+console.log(salary); 
